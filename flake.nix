eaafc3514d1ae4ed5fbfd95ae0d6487854498156
Andrei Mikhailov <amkhlv@gmail.com>
2026-01-10 16:08:49 -0300

    updated flake

--- flake.nix
+++ flake.nix
@@ -3,14 +3,47 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
-  outputs = { self, nixpkgs }:
+
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgsFor = nixpkgs.legacyPackages;
-    in {
+    in
+    {
       packages = forAllSystems (system: {
         default = pkgsFor.${system}.callPackage ./. { };
       });
+
+devShells = forAllSystems (system: {
+  default = pkgsFor.${system}.mkShell {
+    packages = with pkgsFor.${system}; [
+      # Rust tooling
+      rustc cargo rust-analyzer rustfmt clippy
+
+      # Build tooling for -sys crates
+      pkg-config
+      gcc
+
+      # GTK / GLib stack (dev outputs matter)
+      glib.dev
+      gtk3.dev
+      gdk-pixbuf.dev
+      cairo.dev
+      pango.dev
+      atk.dev
+
+      # Often required transitively
+      zlib.dev
+    ];
+  };
+});
+    
     };
 }
